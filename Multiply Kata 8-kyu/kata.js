0eb7f91241109f3bcae81369bf8573fb2525f876
alescofet <alex.escofet96@gmail.com>
2021-04-13 17:51:08 +0200

    Multiply Kata done

---
+++ Multiply Kata 8-kyu/kata.js
@@ -0,0 +1,16 @@
+/*
+Link:  https://www.codewars.com/kata/50654ddff44f800200000004
+
+This code does not execute properly. Try to figure out why.
+
+
+function multiply(a, b){
+    a * b
+  } */
+
+
+  function multiply(a, b){
+    return (a * b)
+  }
+
+
